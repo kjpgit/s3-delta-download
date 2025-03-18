1f4b75028eaf64249a1971f738085c3bdde2d1ec
kjpgit <kjpgit@users.noreply.github.com>
2025-03-18 08:55:30 -0500

    check args

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-﻿foreach (string arg in args) {
-    //Console.WriteLine(arg);
-}
-
+﻿// Required for Native AOT compilation
 Amazon.RuntimeDependencies.GlobalRuntimeDependencyRegistry.Instance.RegisterChecksumProvider(
   new AWSSDK.Extensions.CrtIntegration.CrtChecksums()
   );
 
-var s3util = new S3Util(args[0], args[1]);
+// Check arguments
+if (args.Count() != 3) {
+    Console.WriteLine("Usage: S3DeltaDownload <BUCKETNAME> <KEYPREFIX> <LOCALDIRECTORY>");
+    Environment.Exit(1);
+}
+
+var s3util = new S3Util(bucketName: args[0], keyPrefix: args[1]);
 await foreach (string key in s3util.ListBucketContentsAsync()) {
-    Console.WriteLine(key);
+    //Console.WriteLine(key);
     await s3util.DownloadFile(key, args[2]);
 }
